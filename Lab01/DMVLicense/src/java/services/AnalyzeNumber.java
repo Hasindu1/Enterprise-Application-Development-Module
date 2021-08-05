f370d0d87f85ee1799213e02dda2c7d06952ca3f
Hasindu1 <hasiyadahanayake@gmail.com>
2021-08-05 22:53:49 +0530

    feat(application):add lab01 dmv licnese Java EE web application

---
+++ Lab01/DMVLicense/src/java/services/AnalyzeNumber.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import beans.PlateNumberManager;
+import javax.ejb.EJB;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author Hasindu Dahanayake
+ */
+@Path("scanplate")
+public class AnalyzeNumber {
+
+    @Context
+    private UriInfo context;
+
+    @EJB
+    PlateNumberManager pnm;
+
+    /**
+     * Creates a new instance of AnalyzeNumber
+     */
+    public AnalyzeNumber() {
+    }
+
+    /**
+     * Retrieves representation of an instance of services.AnalyzeNumber
+     *
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String getHtml() {
+        //TODO return proper representation object
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * POST method for capture the input from the html form
+     *
+     * @param content representation for the resource
+     */
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_HTML)
+    public String getCategoryPost(@FormParam("regnum") String registration) {
+        return "<h2> Required Driving License Category :"
+                + pnm.getDLCategory(registration) + "</h2 >";
+
+    }
+
+    @GET
+    @Path("getcategory")
+    @Produces(MediaType.TEXT_HTML)
+    public String
+            getCategoryParams(@QueryParam("regnum") @DefaultValue("CBE1234") String registration) {
+        return "<h2> Required Driving License Category :"
+                + pnm.getDLCategory(registration) + "</h2>";
+    }
+
+    /**
+     * GET method with PATH variables
+     *
+     * @param registration
+     * @return
+     */
+    @GET
+    @Path("{regnum}")
+    @Produces(MediaType.TEXT_HTML)
+    public String getCategoryPath(@PathParam("regnum") String registration) {
+        return "<h2> Required Driving License Category :"
+                + pnm.getDLCategory(registration) + "</h2 >";
+
+    }
+}
